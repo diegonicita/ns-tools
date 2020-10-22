05969f3862d027499a3285eb1c36b2994bb19712
diegonicita <diegonicita@gmail.com>
2020-10-22 17:17:52 -0300

    Add Some New Stuff
    New Things

---
+++ Runtime/Menu/MenuInicial/Botones/MouseOver.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+namespace NicitaSoftware.Tools
+{
+    public class MouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
+    {
+        [SerializeField]
+        Vector3 escalaBase = new Vector3();
+        public float scaleFactor = 1.05f;
+        GameObject shadow;
+
+        public void Awake()
+        {
+            escalaBase = transform.localScale;          
+            shadow = transform.GetChild(0).gameObject;
+            shadow.gameObject.SetActive(false);
+        }
+
+        //Do this when the cursor enters the rect area of this selectable UI object.
+        public void OnPointerEnter(PointerEventData eventData)
+        {            
+            transform.localScale = escalaBase * scaleFactor;           
+            shadow.gameObject.SetActive(true);  
+        }
+
+        //Do this when the cursor enters the rect area of this selectable UI object.
+        public void OnPointerExit(PointerEventData eventData)
+        {           
+            transform.localScale = escalaBase;
+            shadow.gameObject.SetActive(false);
+        }
+
+    }
+}
